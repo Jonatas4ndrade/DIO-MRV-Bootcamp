5b1d8d10170f5938a72c25e65ce58090f64bd04d
JonatasAndrade <jonatassilveiraandrade@gmail.com>
2021-12-27 22:21:00 -0300

    added CSS/Javascript files, modified ally sprite.

---
+++ JogoNaves/jogo1/js/js.js
@@ -0,0 +1,35 @@
+function start() { // Inicio da função start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+	//Principais variáveis do jogo
+	
+	var jogo = {}
+	
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	movefundo();
+	
+	} // Fim da função loop()
+
+
+
+//Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+	
+	esquerda = parseInt($("#fundoGame").css("background-position"));
+	$("#fundoGame").css("background-position",esquerda-1);
+	
+	} // fim da função movefundo()
+
+}
