40c5bda3a379b680c4b68c93b02a10b3bdf5b359
JonatasAndrade <jonatassilveiraandrade@gmail.com>
2021-12-29 23:50:38 -0300

    Score and life bar added. Several sprite improvements made.

--- JogoNaves/jogo1/js/js.js
+++ JogoNaves/jogo1/js/js.js
@@ -1,35 +1,44 @@
 function start() { // Inicio da função start()
-
+	//Hides start menu and appends UI.
 	$("#inicio").hide();
+	$("#fundoGame").append("<div id='placar'></div>");
+	$("#fundoGame").append("<div id='energy'></div>");
+
+
+	//Appends game entities
 	
+
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-	//Principais variáveis do jogo
-	
+	/*** GAME LOOP VARIABLES ***/
+	var gameOver = false;
 	var jogo = {}
+	jogo.pressionou = [];
+
+	var score =0;
+	var safe = 2; //Pilot and narrator :P
+	var lost = 0;
+	var curEnergy=3;
+
+	/*** ENEMY VARIABLES ***/
+	var velocidade = 7; // Enemy chopper speed
+	var velocidade2 =5; // Truck speed
+	var posicaoY = parseInt(Math.random() * 334); // Enemy Vertical Position
+	
+	/*** OTHER VARIABLES***/
+
 	var TECLA = {
 		W: 87,
 		S: 83,
 		D: 68,
 		A: 65
 		}
-	
-	jogo.pressionou = [];
-
-	/*** ENEMY VARIABLES ***/
-	var velocidade = 7; // Enemy speed
-	var posicaoY = parseInt(Math.random() * 334); // Enemy Vertical Position
-	
-	/*** OTHER VARIABLES***/
 
 	var shot1Ready = true;
 	var shot2Ready = true;
 	
-
-
 	//Verifica se o usuário pressionou alguma tecla	
 	//Arrays are already objects, so it is possible assign new properties.
 	
@@ -46,28 +55,31 @@ function start() { // Inicio da função start()
 	
 	function loop() {
 	
+	//Appends new executive if truck not in screen.
+	if ( $("#amigo").length == 0 && parseInt($("#inimigo2").css("left")) > 1200 && gameOver == false) {
+		$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+	}
 	movefundo();
 	movejogador();
 	moveExecutive();
 	moveinimigo1();
 	moveinimigo2();
 	colisao();
-
-
+	gameScore();
+	energyUI();
 	} // Game Loop closed
 
 	/*** FUNCTION DEFINITIONS BELOW ***/
 
-	//Função que movimenta o fundo do jogo
+	/* MOVEMENT */
 	
+	//Função que movimenta o fundo do jogo
 	function movefundo() {
 	
 	esquerda = parseInt($("#fundoGame").css("background-position"));
 	$("#fundoGame").css("background-position",esquerda-2);
 	
-	} // fim da função movefundo()
-	
-	//Verifica se o usuário pressionou alguma tecla	
+	} 
 	
 	function movejogador() {
 		
@@ -84,23 +96,21 @@ function start() { // Inicio da função start()
 		}
 		
 	if (jogo.pressionou[TECLA.D]) {
-		horizShot();
+		missile();
 		}
 
 	if (jogo.pressionou[TECLA.A]) {
-		missileShot();
+		bomb();
 		}
 	} // fim da função movejogador()
 
 	function moveExecutive() {
 	
-		posicaoX = parseInt($("#amigo").css("left"));
-		$("#amigo").css("left",posicaoX+2);
+		posicaoX = parseInt($("#amigo").css("right"));
+		$("#amigo").css("right",posicaoX+4);
 					
-			if (posicaoX>906) {
-				
-			$("#amigo").css("left",0);
-						
+			if (posicaoX> 1000) {
+				$("#amigo").remove(); // Will be added again by game loop().	
 			}
 	
 	} // fim da função moveamigo()
@@ -112,25 +122,26 @@ function start() { // Inicio da função start()
 		$("#inimigo1").css("top",posicaoY);
 			
 			if (posicaoX<=-400) {
-			posicaoY = parseInt(Math.random() * 334);
-			$("#inimigo1").css("left", 900);
-			$("#inimigo1").css("top",posicaoY);
+				score -= 50; // Decrease score if enemy is not destroyed.
+				posicaoY = parseInt(Math.random() * 334);
+				$("#inimigo1").css("left", 900);
+				$("#inimigo1").css("top",posicaoY);
 				
 			}
 	} //Fim da função moveinimigo1()
 
 	function moveinimigo2() {
         posicaoX = parseInt($("#inimigo2").css("left"));
-		$("#inimigo2").css("left",posicaoX-3);
+		$("#inimigo2").css("left",posicaoX-velocidade2);
 				
 		if (posicaoX<=-200) {
-			
-		$("#inimigo2").css("left",775);
-					
+			reposicionaInimigo2()	
 		}
 	} // Fim da função moveinimigo2()
 
-	function horizShot() {
+	/* SHOOTING */
+
+	function missile() {
 	
 		if (shot1Ready) {
 			
@@ -148,7 +159,7 @@ function start() { // Inicio da função start()
 	 
 		function executaDisparo1() {
 		posicaoX = parseInt($("#disparo").css("left"));
-		$("#disparo").css("left",posicaoX+15); 
+		$("#disparo").css("left",posicaoX+22); 
 	
 			if (posicaoX>920) {
 				window.clearInterval(tempoDisparo);
@@ -159,7 +170,7 @@ function start() { // Inicio da função start()
 		} // Fecha executaDisparo()
 	} // Fecha disparo()
 
-	function missileShot() {
+	function bomb() {
 	
 		if (shot2Ready) {
 			
@@ -181,7 +192,7 @@ function start() { // Inicio da função start()
 		$("#missile").css({ "top": posiY + 11, "left": posicaoX + 15});
 		
 		if (posiY < 400) //Stops rotation when next to the floor. More natural motion.
-			$("#missile").css("transform", "rotate("+ posicaoX / 9 +"deg)");
+			$("#missile").css("transform", "rotate("+ posicaoX / 10 +"deg)");
 		
 			if (posiY > 464) {
 				window.clearInterval(tempoDisparo);
@@ -193,41 +204,123 @@ function start() { // Inicio da função start()
 		} // Fecha executaDisparo()
 	} // Fecha disparo()
 	
+	/* COLISION DETECTION */
+
 	function colisao() {
-		var colisao1 = ($("#jogador").collision($("#inimigo1")));
+		var colPlayerChopper = ($("#jogador").collision($("#inimigo1")));
+		var colPlayerTruck = ($("#jogador").collision($("#inimigo2")));
+		var chopperHit = ($("#disparo").collision($("#inimigo1")));
+		var truckHit = ($("#disparo").collision($("#inimigo2")));
+		var chopperBomb = ($("#missile").collision($("#inimigo1")));
+		var truckBomb = ($("#missile").collision($("#inimigo2")));
+		var rescued = ($("#jogador").collision($("#amigo")));
+		var ranOver = ($("#inimigo2").collision($("#amigo")));
+		
 		// jogador com o inimigo1
 			
-			if (colisao1.length>0) {
+			if (colPlayerChopper.length>0) {
 				
 			inimigo1X = parseInt($("#inimigo1").css("left"));
 			inimigo1Y = parseInt($("#inimigo1").css("top"));
 			explosao1(inimigo1X - 65,inimigo1Y);
 		
 			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",1150);
+			$("#inimigo1").css("top",posicaoY);
+				
+			playerDown(); 
+			curEnergy--;
+			}
+
+		// jogador com o inimigo2 
+			if (colPlayerTruck.length>0) {
+				
+			playerDown();
+			curEnergy--;
+			reposicionaInimigo2();
+				
+			}	
+
+		// Disparo com o inimigo1
+				
+			if (chopperHit.length>0 || chopperBomb.length>0) {
+								
+			inimigo1X = parseInt($("#inimigo1").css("left"));
+			inimigo1Y = parseInt($("#inimigo1").css("top"));
+						
+			explosao1(inimigo1X,inimigo1Y);
+			score += 100;
+			velocidade += 1;
+			//Displace projectiles
+			if (chopperHit.length>0)
+			$("#disparo").css("left",950);
+			if (chopperBomb.length>0)
+			$("#missile").css("top",1000);
+				
+			posicaoY = parseInt(Math.random() * 334);
 			$("#inimigo1").css("left",920);
 			$("#inimigo1").css("top",posicaoY);
-			}
+				
+			}	
 		
+		// Disparo com o inimigo2
+				
+			if (truckHit.length>0 || truckBomb.length>0) {
+				
+				//Displace projectiles
+				if (truckHit.length>0)
+				$("#disparo").css("left",950);
+				if (truckBomb.length>0)
+				$("#missile").css("top",1000);
+
+				reposicionaInimigo2();
+				score += 50;
+				velocidade += 0.5;
+			}		
+
+		// Player and Ally. Will only rescue if within door range.
+		
+			if (rescued.length>0 && parseInt($("#amigo").css("right")) > 770) {
+				$("#amigo").remove();
+				score += 200;
+				safe++;
+			}
+		// Enemy and Ally			
+			if (ranOver.length>0) {
+				explosao3();
+				$("#amigo").remove();
+				score -= 500;
+				lost++;
+			}
+			
 		} //Fim da função colisao()
 
-		//Explosão 1
-	function explosao1(inimigo1X,inimigo1Y) {
-		//Add explosion and broken glass screen entities.
+	/* EXPLOSIONS */
+
+	function playerDown() {
+		//If a collision already exists and element is transparent
+		$("#brokenScreen").remove();
+		//Add explosion and broken glass entity.
 		$("#fundoGame").append("<div id='brokenScreen'></div");
+		//Background added via jquery, compatibility issues.
+		$("#brokenScreen").css("background-image", "url(imgs/broken_screen.png)");
+		//Add player flashing effect on hit.
+		$("#jogador").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+		//Fades broken screen effect.
+		$("#brokenScreen").animate({opacity:0}, 1000);
+			
+		}
+
+	function explosao1(inimigo1X,inimigo1Y) {
+		//Add explosion entity.
 		$("#fundoGame").append("<div id='explosao1'></div");
 		
 		//Background added via jquery, compatibility issues.
-		$("#brokenScreen").css("background-image", "url(imgs/broken_screen.png)");
 		$("#explosao1").css("background-image", "url(imgs/explosao.png)");
-		
-		//Add player flashing effect on hit.
-		$("#jogador").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 		
 		//Arguably improves readability
 		var exp=$("#explosao1");
-		var glass=$("#brokenScreen")
-
-		glass.animate({opacity:0}, 1000);
+		
 		exp.css("top", inimigo1Y);
 		exp.css("left", inimigo1X);
 		exp.animate({width:200, opacity:0}, "slow");
@@ -238,11 +331,101 @@ function start() { // Inicio da função start()
 			function removeExplosao() {
 			
 				exp.remove();
-				glass.remove();
 				window.clearInterval(tempoExplosao);
 				tempoExplosao=null;
 			
 			}
 		
 	} // Fim da função explosao1()
+	
+	//Truck Explosion
+	function explosao2(inimigo2X,inimigo2Y) {
+	
+		$("#fundoGame").append("<div id='explosao2'></div");
+		$("#explosao2").css("background-image", "url(imgs/explosao.png)");
+		var div2=$("#explosao2");
+		div2.css("top", inimigo2Y);
+		div2.css("left", inimigo2X +20);
+		div2.animate({width:200, opacity:0}, "slow");
+		
+		var tempoExplosao2=window.setInterval(removeExplosao2, 1000);
+		
+			function removeExplosao2() {
+				
+				div2.remove();
+				window.clearInterval(tempoExplosao2);
+				tempoExplosao2=null;
+				
+			}
+				
+		} // Fim explosao2()
+
+	//The executive meets his demise. The client will hear of his bravery.
+	function explosao3() {
+		$("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+		$("#explosao3").css("left", parseInt($("#amigo").css("left")));
+		var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+		
+			function resetaExplosao3() {
+				$("#explosao3").remove();
+				window.clearInterval(tempoExplosao3);
+				tempoExplosao3=null;
+			}
+	}
+	/* UI FUNCTIONS */
+
+	//Score
+	function gameScore() {
+		$("#placar").html("<h2> Score: " + score + " | Attending Meeting: " + safe + " | Terminated: " + lost + "</h2>");
+		
+	} 
+
+	function energyUI() {
+	
+		if (curEnergy==3) {
+			$("#energy").css("background-image", "url(imgs/energia3.png)");
+		}
+	
+		if (curEnergy==2) {
+			$("#energy").css("background-image", "url(imgs/energia2.png)");
+		}
+	
+		if (curEnergy==1) {
+			$("#energy").css("background-image", "url(imgs/energia1.png)");
+		}
+	
+		if (curEnergy==0) {
+			$("#energy").css("background-image", "url(imgs/energia0.png)");
+			gameOver == true;			
+		}
+	
+	} // Fim da função energia()
+
+	/* OTHER FUNCTIONS */
+
+	//Reposiciona Inimigo2
+	function reposicionaInimigo2() {
+	
+		inimigo2X = parseInt($("#inimigo2").css("left"));
+		inimigo2Y = parseInt($("#inimigo2").css("top"));
+		explosao2(inimigo2X,inimigo2Y);
+				
+		$("#inimigo2").remove();
+
+		var truckRespawner=window.setInterval(respawnTruck, 1000);
+			
+			function respawnTruck() {
+			window.clearInterval(truckRespawner);
+			truckRespawner=null;
+				
+				if (gameOver==false) {
+				//Truck will respawn from 1200 ~ 2200 px from left
+				$('#inimigo2').css("left", (Math.random() * 1000 + 1200))
+				$("#fundoGame").append("<div id=inimigo2></div");
+				
+				}
+				
+			}	
+		}	
+	
 }
